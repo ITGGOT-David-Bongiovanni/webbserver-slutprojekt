0ac27a02d1822f939ecb2a6584beafab606affbc
ITGGOT-David-Bongiovanni <david.johanssonbongiovanni@itggot.se>
2018-04-26 10:42:37 +0200

    färdig
    Lägga till contacter funktionen är nu klar

--- app.rb
+++ app.rb
@@ -21,16 +21,25 @@ class App < Sinatra::Base
 		db = SQLite3::Database.new("db/databas.db")
 		username = params["username"]
 		password = params["password"]
-		user_id = db.execute("SELECT id FROM users WHERE username='#{username}'")
-		password_digest = db.execute("SELECT password FROM users WHERE username='#{username}'").join
+		if username == "" or password == ""
+			session[:message] = "Wrong Password or Username"
+			redirect('/error')
+		end
+		begin
+		user_id = db.execute("SELECT id FROM users WHERE username=?", [username])
+		password_digest = db.execute("SELECT password FROM users WHERE username=?", [username]).join
 		password_digest = BCrypt::Password.new(password_digest)
+		rescue
+			session[:message] = "Wrong Password or Username"
+			redirect('/error')
+		end
 		if password_digest == password
 			session[:username] = username
 			session[:id] = user_id
-
 			redirect('/list')
 		else
-			redirect('/register')
+			session[:message] = "Wrong Password or Username"
+			redirect('/error')
 		end
 	end
 
@@ -71,7 +80,7 @@ class App < Sinatra::Base
 			redirect('/login')
 		else
 			user_id = session[:id] 
-			contacts = db.execute("SELECT username,telephone FROM users WHERE id IN (SELECT contact_id FROM contact WHERE user_id = ?)", [user_id])
+			contacts = db.execute("SELECT username,telephone,id FROM users WHERE id IN (SELECT contact_id FROM contact WHERE user_id = ?)", [user_id])
 			slim(:list, locals:{contacts:contacts})
 		end
 	end
@@ -81,9 +90,31 @@ class App < Sinatra::Base
 			redirect('login')
 		else
 			user_id = session[:id]
-			all = db.execute("SELECT username,telephone FROM users WHERE id IS NOT (?)", [user_id]) 
+			all = db.execute("SELECT username,telephone,id FROM users WHERE id IS NOT (?)", [user_id]) 
 			# ändra så man inte får med favoriter
 			slim(:add, locals:{all:all})
+		end
+	end
+	post ('/add') do
+		db = SQLite3::Database.open("db/databas.db")
+		if session[:id] == nil
+			redirect('login')
+		else
+			user_id = session[:id]
+			contact_id = params["add_user_id"]
+			db.execute("INSERT INTO contact (user_id, contact_id) VALUES (?,?)", [user_id, contact_id])
+			redirect('/list')
+		end
+	end
+	post('/remove') do
+		db = SQLite3::Database.open("db/databas.db")
+		if session[:id] == nil
+			redirect('login')
+		else
+			user_id = session[:id]
+			contact_id = params["add_user_id"]
+			db.execute("DELETE FROM contact WHERE (user_id=? AND contact_id=?)", [user_id, contact_id])
+			redirect('/list')
 		end
 	end
 end           
